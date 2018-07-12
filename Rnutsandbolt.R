00fd53da516d26e4c5ed2c140abf5b678902e28f
daniellequijano <daniellequijano0315@gmail.com>
2018-07-12 15:53:30 +0800

    ver2

--- Rnutsandbolt.R
+++ Rnutsandbolt.R
@@ -11,7 +11,7 @@ z
 
 x <-c(0.7,0.8)
 x
-<-c(TRUE,FALSE)
+x <-c(TRUE,FALSE)
 x
 x<-c(T,F)
 x
@@ -73,4 +73,92 @@ unclass(x)
 
 x<- factor(c("Shikai","Bankai","Shikai","Bankai","Shikai","Bankai","Shikai"), levels = c("Shikai","Bankai"))
 x
+
+##MISSING VALUES
+
+x<-c(1,2,NA,10,3)
+is.na(x)
+
+is.nan(x)
+
+x<-c(1,2,NaN, NA, 4)
+is.na(x)
+
+is.nan(x)
+
+##DATAFRAMES
+x<- data.frame(foo = 1:4, bar= c(T,T,F,F))
+x
+
+nrow[x]
+
+ncol[x]
+
+
+##NAMES
+x<- 1:3
+names(x)
+
+names(x) <- c("foo","bar","roof")
+x
+
+names(x)
+
+x<- list(a=1,b=2,c=3)
+x
+
+m<- matrix(1:4,nrow=2,ncol=2)
+dimnames(m) <- list (c("a", "b"), c("c","d"))
+m
+
+##DPUTTING
+
+y<- data.frame(a=1,b="a")
+dput(y)
+
+dput(y, file = "y.R")
+new.y <- dget("y.R")
+new.y
+
+##DUMPING 
+
+x<- "foo"
+y <- data.frame(a=1, b="a")
+rm(x,y)
+source("data.R")
+y
+
+x
+
+##SUBSETTING
+
+x<- c("a","b","c","d","a")
+x[1]
+
+x[2]
+
+x[1:4]
+
+x[x>"a"]
+
+u <- x > "a"
+u
+
+x[u]
+
+##SUBSETTING Lists
+
+x<-list(foo=1:4, bar=0.6)
+x[1]
+
+x[[1]]
+
+x$bar
+
+x[["bar"]]
+
+x["bar"]
+
+x<- list(foo=1:4, bar=0.6, baz="hello")
+x[c(1,3)]
 
